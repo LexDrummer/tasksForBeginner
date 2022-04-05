f6a3606f96fef49b5cfea2e18a00211bcd793bfb
Alexandr Zarubin <lex.koenig.drummer@gmail.com>
2022-04-05 19:18:31 +0200

    add variables class and first task

---
+++ src/main/java/ru/lexdrummer/variables/Variables.java
@@ -0,0 +1,12 @@
+package main.java.ru.lexdrummer.variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+    // объяви пару переменных z и v типа int и сразу проинициируй (задай значения) их производными числами
+
+        //enter your code below...
+
+        System.out.println("Сумма двух чисел будет равна: " + (z + y));
+    }
+}
