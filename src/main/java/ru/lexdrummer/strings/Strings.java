bc6c9e96a0bbbc7626779e60edc035c166ffa747
Alexandr Zarubin <lex.koenig.drummer@gmail.com>
2022-04-06 19:05:14 +0200

    add task for Strings lesson

---
+++ src/main/java/ru/lexdrummer/strings/Strings.java
@@ -0,0 +1,17 @@
+package main.java.ru.lexdrummer.strings;
+
+public class Strings {
+    public static void main(String[] args) {
+        /*
+        1. Объяви строковую переменную first типа String и проинициализируй ее произвольным значением.
+            Объяви еще одну строковую переменную second.
+            Выведи в консоль строку - результат сложения двух переменных.
+        2. Объяви переменную nubmer типа int. Выведи в консоль результат сложения переменной number и first
+         */
+
+        // 1. enter your code below...
+
+        // 2. enter your code below...
+
+    }
+}
