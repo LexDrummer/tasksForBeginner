672c130a893a21cc9144a3f26c7219beff78b001
Globalicus <simpsons1992@mail.ru>
2022-04-12 13:01:26 +0200

    Strings is done Variables is done loops is done if- done input - done do while - save progress

---
+++ src/main/java/ru/lexdrummer/do_while.java
@@ -0,0 +1,10 @@
+package main.java.ru.lexdrummer;
+import java.util.Scanner;
+
+public class do_while {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+
+    }
+}
