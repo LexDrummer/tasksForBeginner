36ec56964fce1de99e4a0adc76805d86f471cf28
Globalicus <simpsons1992@mail.ru>
2022-04-12 20:26:56 +0200

    Break&Continue

--- src/main/java/ru/lexdrummer/Break_Continue.java
+++ src/main/java/ru/lexdrummer/Break_Continue.java
@@ -2,6 +2,14 @@ package main.java.ru.lexdrummer;
 
 public class Break_Continue {
     public static void main(String [] args){
-
+        int i = 0;
+    while (true){
+        if(i==15){
+            break;
+        }
+    System.out.println(i);
+    i++;
+}
+    System.out.println("Мы вышли из цикла");
     }
 }
