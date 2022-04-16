7c2fbd7374964e96df857308356016f3e9b9721e
Globalicus <simpsons1992@mail.ru>
2022-04-16 13:26:26 +0200

    Break&Continue- done Switch - done

---
+++ src/main/java/ru/lexdrummer/Switch.java
@@ -0,0 +1,24 @@
+package main.java.ru.lexdrummer;
+
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String [] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи свой возраст");
+        String age = scanner.nextLine(); // Int age = scanner.nextInt(); можно задать как строку так и числовое значение
+        switch(age){
+            case "ноль" :
+                System.out.println("Ты родился");
+                 break;
+            case "семь" :
+                System.out.println("Ты пошел в школу");
+                break;
+            case "восемнадцать" :
+                System.out.println("Ты закончил школу");
+                break;
+            default:
+                System.out.println("Ни одно из условий не подошло");
+        }
+    }
+}
