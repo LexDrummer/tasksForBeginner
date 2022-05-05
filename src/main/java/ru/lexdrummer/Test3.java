e4c73b69c8c3317374efc90e299553e466c30521
Globalicus <simpsons1992@mail.ru>
2022-05-05 14:54:34 +0200

    Switch - done

---
+++ src/main/java/ru/lexdrummer/Test3.java
@@ -0,0 +1,23 @@
+package main.java.ru.lexdrummer;
+
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            String s2=sc.next();
+            int x2 = sc.nextInt();
+            String s3 = sc.next();
+            int x3 = sc.nextInt();
+            //Complete this line
+
+        }
+        System.out.println("================================");
+
+    }
+}
