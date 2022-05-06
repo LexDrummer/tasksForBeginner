6d97a6706758c7c5a40be843c4e8a5bad2b5d8e7
Globalicus <simpsons1992@mail.ru>
2022-05-06 13:50:51 +0200

    Switch - done

--- src/main/java/ru/lexdrummer/Test3.java
+++ src/main/java/ru/lexdrummer/Test3.java
@@ -3,20 +3,41 @@ package main.java.ru.lexdrummer;
 import java.util.Scanner;
 
 public class Test3 {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("================================");
-        for(int i=0;i<3;i++)
-        {
-            String s1=sc.next();
-            int x=sc.nextInt();
-            String s2=sc.next();
-            int x2 = sc.nextInt();
-            String s3 = sc.next();
-            int x3 = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       // System.out.println("================================");
+        for (int i = 0; i < 3; i++) {
+            String s1 = sc.next();
+            int x = sc.nextInt();
+           // sc.nextLine();
+            if ( i < 1 ) {
+                System.out.println();
+                System.out.println("================================");
+            }
+            /*
+             java 100
+ddp 50
+pyton 34
+
+
+
+            *
+            * */
+            String zero;
+            if (x < 100) {
+                if (x<10){
+                    zero = "00";
+                } else {
+                    zero = "0";
+                }
+            } else { zero = "";
+            }
+            System.out.printf("%-15s %-15s\n", s1, zero+x);
+
             //Complete this line
 
         }
+        sc.close();
         System.out.println("================================");
 
     }
