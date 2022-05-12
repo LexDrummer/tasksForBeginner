a466506c7e181b50cd7dc80264562a2a8900aa4c
Globalicus <simpsons1992@mail.ru>
2022-05-12 15:41:24 +0200

    Switch - done

---
+++ src/JavaLessons/ClassesAndObjects.java
@@ -0,0 +1,25 @@
+package JavaLessons;
+
+public class ClassesAndObjects {      // основной класс должен соответствовать названию файла, он только один
+    public static void main(String[] args) { //
+        Person person1 = new Person();
+        person1.name = "Рик";
+        person1.age = 50;
+        System.out.println("Меня зовут "+person1.name+", "+"мне "+person1.age+" лет"+" и лососни тунца.");
+        Person person2 = new Person();
+        person2.name = "Морти";
+        person2.age = 16;
+        System.out.println("Меня зовут "+person2.name+", "+"мне "+person2.age+" лет"+". Я люблю Джессику.");
+    }
+
+}
+class test{   // дополнительный вспомогательный класс их может быть много
+
+}
+class Person{
+    // У класса может быть:
+    // 1.Данные (поля)
+    // 2.Действия, которые он может соверщать (методы)
+    String name;
+    int age;
+}
